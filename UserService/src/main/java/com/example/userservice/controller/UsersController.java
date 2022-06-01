a305c946263c6fb461333ae7bd918df25bf4b0f1
tobey <fafoworatobi25@gmail.com>
2022-06-01 10:53:08 +0100

    refactor

--- UserService/src/main/java/com/example/userservice/controller/UsersController.java
+++ UserService/src/main/java/com/example/userservice/controller/UsersController.java
@@ -1,4 +1,4 @@
-package com.example.userservice;
+package com.example.userservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
